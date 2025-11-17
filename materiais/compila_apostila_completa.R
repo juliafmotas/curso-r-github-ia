6e4b503877bce872e4b858865b2c842ee2d523e1
Vinicius Junqueira <junqueiravinicius@hotmail.com>
2025-11-17 13:30:03 -0300

    html pages

--- materiais/compila_apostila_completa.R
+++ materiais/compila_apostila_completa.R
@@ -1,2 +1,10 @@
 setwd('./materiais/')
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+
+setwd('./materiais/')
+
+# Para formato GitBook (recomendado para web)
+bookdown::render_book("index.Rmd", "bookdown::gitbook")
+
+# OU para formato BS4 (Bootstrap 4 - mais moderno)
+# bookdown::render_book("index.Rmd", "bookdown::bs4_book")
