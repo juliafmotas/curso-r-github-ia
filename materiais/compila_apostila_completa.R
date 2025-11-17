f0df552195cf9b3940dfa3e4df77a31a9c5d3b58
Vinicius Junqueira <junqueiravinicius@hotmail.com>
2025-11-17 14:26:20 -0300

    pages

--- materiais/compila_apostila_completa.R
+++ materiais/compila_apostila_completa.R
@@ -5,10 +5,5 @@ bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 # Para formato GitBook (recomendado para web)
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 
-system('cp -rf materiais/_book/* docs/')
-
-# Verificar se a pasta docs existe e tem arquivos
-list.files("docs")
-
-# No R, na raiz do projeto
-file.create(".nojekyll")
+setwd('/Users/viniciusjunqueira/Library/CloudStorage/OneDrive-Pessoal/Cursos/curso-r-github-ia')
+system('sh copy_html.sh')
